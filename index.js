a761565838ecab86e69eabdcb9217acfad41b856
Pradip Gharge <59740723+pradipgharge@users.noreply.github.com>
2021-08-15 17:38:04 +0530

    Update: added Quiz rules

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ var emoji = require('node-emoji')
 //Accept user name
 var userName = readlineSync.question("Please enter your name: ");
 console.log(chalk.red.bgGreen.bold(chalk.red("\n Welcome " + userName.toUpperCase()) + ", Let's play DO YOU KNOW PRADIP ? \n"));
+
+//Explain the rules
+console.log(chalk.bold("Following are the rules of this quiz game:\n"));
+console.log(chalk.cyanBright("1.There are fifteen questions in this game.\n2.For every correct answer you will be awarded 2 points \n3.For every wrong answer 1 point will be deducted.\n4.There is no time limit, but it shouldn't take you long if you know me well!"));
+
+var userScore = 0;
+console.log("-----------------------------------------------------------------------------\n")
+console.log(chalk.black.bold.bgYellowBright(" HERE COME THE QUESTIONS--- \n"))
