148af64358bffe0950cd728a230bfb9d57458e6a
Pradip Gharge <59740723+pradipgharge@users.noreply.github.com>
2021-09-29 16:21:21 +0530

    changed background of text

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var emoji = require('node-emoji')
 
 //Accept user name
 var userName = readlineSync.question("Please enter your name: ");
-console.log(chalk.red.bgGreen.bold(chalk.red("\n Welcome " + userName.toUpperCase()) + ", Let's play DO YOU KNOW PRADIP ? \n"));
+console.log(chalk.red.bgWhite(chalk.red("\n Welcome " + userName.toUpperCase()) + ", Let's play DO YOU KNOW PRADIP ? \n"));
 
 //Explain the rules
 console.log(chalk.bold("Following are the rules of this quiz game:\n"));
